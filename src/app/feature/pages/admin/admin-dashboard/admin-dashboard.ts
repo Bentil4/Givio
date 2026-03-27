77933a1519a71b587c78e64c443026622eb99ef9
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-03-27 15:13:09 +0000

    feat: add quick actions component to admin dashboard; update styles and integrate into template

--- src/app/feature/pages/admin/admin-dashboard/admin-dashboard.ts
+++ src/app/feature/pages/admin/admin-dashboard/admin-dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { StatCard } from '../../../components/stat-card/stat-card';
 import { RecentActivity } from '../../../components/recent-activity/recent-activity';
+import { QuickActions } from "../../../components/quick-actions/quick-actions";
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [StatCard, RecentActivity],
+  imports: [StatCard, RecentActivity, QuickActions],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.scss',
 })
